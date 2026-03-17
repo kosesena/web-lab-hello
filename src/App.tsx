4e19eb57f958208bb38774aa3262b51389dd0cde
Sena <kosesena@hotmail.com>
2026-03-17 04:04:18 +0300

    feat: add personal details to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
       <h2>LAB-1</h2>
       <p>Ad Soyad: Sena Kose</p>
       <p>Ogrenci No: 235542006</p>
+      <p>Bolum: Yazilim Muhendisligi</p>
+      <p>Universite: Firat Universitesi</p>
+      <p>Hobiler: Yazilim gelistirme, mobil uygulama tasarimi</p>
     </div>
   )
 }
