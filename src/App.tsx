a7ab7ea2690ee6a5ab0bbea260cd99aedd29ef48
Sena <kosesena@hotmail.com>
2026-03-17 04:03:41 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Sena Kose</p>
+      <p>Ogrenci No: 235542006</p>
+    </div>
+  )
+}
+
+export default App
